467f948d25d53b6b4c1d4f371322d6bf3e0af4b9
Mugambidev <imugambi647@gmail.com>
2026-04-19 14:48:11 +0300

    fix: Vercel build with dual Prisma schemas

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['@prisma/client'],
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@prisma/client')
+    }
+    return config
   },
 }
 
